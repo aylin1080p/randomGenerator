c77ceece69f7115d7582d8c337e7d5e53d9328fa
aylin gunes <4ylingunes@gmail.com>
2023-02-04 12:47:49 +0100

    add a function named loveMeter

--- loveMeter-RandomGenerator.playground/Contents.swift
+++ loveMeter-RandomGenerator.playground/Contents.swift
@@ -1,7 +1,6 @@
 import UIKit
 
 var greeting = "Hello, random generator"
-
     var numberr = 5
     func randomNumber() {
         let randomNum = Int.random(in: 0...100)
@@ -38,7 +37,7 @@ func loveMeter (){
     let howMuchLove = Int.random(in: 0...100)
     print(howMuchLove)
     if howMuchLove >= 80 {
-        print("youre really love eachother")
+        print("score is : \(howMuchLove) youre really love eachother")
     }else if  howMuchLove > 40 && howMuchLove < 80{
         print("meh")
     } else{
@@ -50,4 +49,45 @@ print("3- love meter running : ")
 for _ in 1...10 {
     loveMeter()
 }
+ // random number with switch case
+let randomNum = Int.random(in: 0..<10)
+
+switch randomNum {
+case 0:
+    print("The number is 0")
+case 1:
+    print("The number is 1")
+case 2:
+    print("The number is 2")
+case 3:
+    print("The number is 3")
+default:
+    print("The number is not 0, 1, 2, or 3")
+}
+
+
+var myEggHardness:Int?
+for i in 1...5 {
+    print("number is : \(i)")
+    myEggHardness = i
+
+switch myEggHardness {
+case 1:
+    print("you waited 1 only minutes")
+    
+
+case 2:
+    print("you waited 2 only minutes")
+    
+case 3:
+    print("you waited 3 only minutes")
+    
+default:
+    print("ready")
+}
+    
+}
+
+print("Enter a number:")
+
 
